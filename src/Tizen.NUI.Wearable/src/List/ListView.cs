4fc436fba1b9c3f9af0f9e409c6141004761faa2
neostom432 <minho.sun@samsung.com>
2020-05-14 15:00:04 +0900

    [NUI] Notice before scrolling Animation Ended

--- src/Tizen.NUI.Wearable/src/List/ListView.cs
+++ src/Tizen.NUI.Wearable/src/List/ListView.cs
@@ -15,13 +15,10 @@ namespace Tizen.NUI.Wearable
     [EditorBrowsable(EditorBrowsableState.Never)]
     public class ListView : RecyclerList
     {
+        private ListItem FocusedItem = null;
         public ListView(ListAdapter adapter) : base(adapter, new FishEyeLayoutManager())
         {
             ScrollingDirection = ScrollableBase.Direction.Vertical;
-
-            mContainer.PositionUsesPivotPoint = true;
-            mContainer.ParentOrigin = Tizen.NUI.ParentOrigin.Center;
-            mContainer.PivotPoint = ScrollingDirection == Direction.Vertical?Tizen.NUI.PivotPoint.TopCenter:Tizen.NUI.PivotPoint.CenterLeft;
 
             ScrollDragStartEvent += OnScrollDragStart;
             ScrollAnimationEndEvent += OnAnimationEnd;
@@ -32,30 +29,41 @@ namespace Tizen.NUI.Wearable
                 child.ParentOrigin = Tizen.NUI.ParentOrigin.TopCenter;
             }
 
+            mContainer.PositionUsesPivotPoint = true;
+            mContainer.ParentOrigin = Tizen.NUI.ParentOrigin.Center;
+            mContainer.PivotPoint = ScrollingDirection == Direction.Vertical?Tizen.NUI.PivotPoint.TopCenter:Tizen.NUI.PivotPoint.CenterLeft;
+            mContainer.Size = new Size(mLayoutManager.StepSize*(mAdapter.Data.Count-1),mContainer.Size.Height);
+            ScrollAvailableArea = new Vector2(0,mLayoutManager.StepSize*(mAdapter.Data.Count-1));
+            noticeAnimationEndBeforePosition = 100;
+
             ListItem focusedItem = mContainer.Children[0] as ListItem;
             focusedItem.OnFocusGained();
         }
 
         private void OnAnimationEnd(object source, ScrollableBase.ScrollEventArgs args)
         {
-            FishEyeLayoutManager layoutManager = mLayoutManager as FishEyeLayoutManager;
+        }
 
-            if(layoutManager != null)
+        protected override void OnPreReachedTargetPosition(float targetPosition)
+        {
+            int targetDataIndex = (int)(Math.Abs(targetPosition) / mLayoutManager.StepSize);
+
+            for(int i = 0 ; i<mContainer.Children.Count;i++)
             {
-                ListItem focusedItem = mContainer.Children[layoutManager.FocusedIndex] as ListItem;
-                focusedItem.OnFocusGained();
+                ListItem item = mContainer.Children[i] as ListItem;
+
+                if(targetDataIndex == item.DataIndex)
+                {
+                    FocusedItem = item;
+                    item.OnFocusGained();
+                    break;
+                }
             }
         }
 
         private void OnScrollDragStart(object source, ScrollableBase.ScrollEventArgs args)
         {
-            FishEyeLayoutManager layoutManager = mLayoutManager as FishEyeLayoutManager;
-
-            if(layoutManager != null)
-            {
-                ListItem focusedItem = mContainer.Children[layoutManager.FocusedIndex] as ListItem;
-                focusedItem.OnFocusLost();
-            }
+            FocusedItem?.OnFocusLost();
         }
     }
 }
